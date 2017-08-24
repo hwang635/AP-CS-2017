3a0c3810e7edc78140842bf8be68b9a6caae153f
phil <phil@LenovoLark.Hemiwaly.com>
2017-08-23 21:05:41 -0700

    drew person

--- DrawHouse/src/Person.java
+++ DrawHouse/src/Person.java
@@ -7,7 +7,17 @@ public class Person {
 	
 	public void draw(PApplet drawer) {
 		drawer.stroke(0);
-		drawer.ellipse(50, 50, 100, 100);
+		
+		//head
+		drawer.ellipse(50, 350, 50, 50);
+		//body
+		drawer.line(50, 375, 50, 420);
+		//arms
+		drawer.line(50, 390, 65, 385);
+		drawer.line(50, 390, 35, 385);
+		//legs
+		drawer.line(50, 420, 70, 445);
+		drawer.line(50, 420, 30, 445);
 	}
 
 }
