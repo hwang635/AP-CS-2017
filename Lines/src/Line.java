e1511dc8ced99ed71e9f2b3aff48dc7c56f6d7d5
phil <phil@LenovoLark.Hemiwaly.com>
2017-08-31 21:08:20 -0700

    line intersect

--- Lines/src/Line.java
+++ Lines/src/Line.java
@@ -25,16 +25,72 @@ public class Line {
 	}
 	
 	public boolean intersects(Line other) {
+		//storing which x/y is lowest/furtherest left/etc
+		int xLeft1, xLeft2, xRight1, xRight2;
+		int yUp1, yUp2, yDown1, yDown2;
+		
+		boolean check1 = false;
+		boolean check2 = false;
+		
 		this.x3 = other.x1;
 		this.x4 = other.x2;
 		this.y3 = other.y1;
 		this.y4 =  other.y2;
 		
-		pointX = (int) (((x1*y2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)));
+		//if x1 is more right than x2 ==> x2 is left, to use later
+		// or if same, doesn't matter
+		if(x1>=x2) {
+			xLeft1 = (int) x2;
+			xRight1 = (int) x1;
+		}
+		else {
+			xLeft1 = (int) x1;
+			xRight1 = (int) x2;
+		}
+		if(x3>=x4) {
+			xRight2 = (int) x3;
+			xLeft2 = (int) x4;
+		}
+		else {
+			xRight2 = (int) x4;
+			xLeft2 = (int) x3;
+		}
+			
+		//same for y
+		if(y1>=y2) {
+			yUp1 = (int) y1;
+			yDown1 = (int) y2;
+		}
+		else {
+			yUp1 = (int) y2;
+			yDown1 = (int) y1;
+		}
+		if(y3>=y4) {
+			yUp2 = (int) y3;
+			yDown2 = (int) y4;
+		}
+		else {
+			yUp2 = (int) y4;
+			yDown2 = (int) y3;
+		}
+		
+		pointX = (int) (((x1*y2-y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)));
 		pointY = (int) (((x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)));
 		
-		//if(pointX > this.x1 && pointX < this.x2 && pointX 
-		return false;
+		if(pointX >= xLeft1 && pointX <= xRight1) {
+			if(pointY <= yUp1 && pointY >= yDown1)
+				check1 = true;
+		}
+		
+		if(pointX >= xLeft2 && pointX <= xRight2) {
+			if(pointY <= yUp2 && pointY >= yDown2)
+				check2 = true;
+		}
+		
+		if(check1 == true && check2 == true)
+			return true;
+		else
+			return false;
 	}
 
 }
