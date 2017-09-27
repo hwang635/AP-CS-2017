3a1420b2a772faee7a8c4e11c5bc2cce8d8e1582
phil <phil@10.16.48.107>
2017-09-26 18:45:28 -0700

    o.o

--- Illusion/src/DrawingSurface.java
+++ Illusion/src/DrawingSurface.java
@@ -1,13 +1,16 @@
+import helen.shapes.Line;
 import helen.shapes.Rectangle;
 import processing.core.PApplet;
 
 public class DrawingSurface extends PApplet{
 	
 	private Rectangle whiteBackground, blackBackground;
+	private Rectangle vertical;
+	private Line lines;
 	
 	public DrawingSurface() {
-		blackBackground = new Rectangle(0, 0, 700, 500);
-		whiteBackground = new Rectangle(0,0, 620, 500);
+		blackBackground = new Rectangle(0, 0, 630, 500);
+		whiteBackground = new Rectangle(0,0, 500, 500);
 	}
 	
 	public void draw() {
@@ -17,6 +20,25 @@ public class DrawingSurface extends PApplet{
 		whiteBackground.changeColour(true);
 		whiteBackground.setColour(255);
 		whiteBackground.draw(this);
+		
+		lines = new Line(250, 250, 250, 70);
+		lines.setThickness(7);
+		lines.setColour(50);
+		lines.draw(this);
+		
+	
+	
+		
+		//vertical == the six rectangles at the front of the illusion
+		vertical = new Rectangle(76, 70, 10, 350);
+		vertical.setColour(50);
+		vertical.draw(this);
+		
+		//for loop to shift then draw the rectangle 6 times
+		for(int i = 0; i<5; i++) {
+			vertical.translate(68, 0);
+			vertical.draw(this);
+		}
 	
 		
 	}
