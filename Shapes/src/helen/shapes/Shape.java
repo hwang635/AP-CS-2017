6cc12b332a9f6272361f0cfd32248bf4bc51a098
phil <phil@LenovoLark.Hemiwaly.com>
2017-09-27 22:08:31 -0700

    um

--- Shapes/src/helen/shapes/Shape.java
+++ Shapes/src/helen/shapes/Shape.java
@@ -3,7 +3,11 @@ package helen.shapes;
 import processing.core.PApplet;
 
 public class Shape {
-	
+
+	private double x, y, area, perimeter;
+	private int r, g, b;
+	private boolean isGrey;
+
 	public double calcArea() {
 		return 0;
 	}
@@ -14,4 +18,12 @@ public class Shape {
 
 	public void draw(PApplet drawer) {
 	}
+
+	public void changeColour(boolean isGrey) {
+
+	}
+
+	public void setColour(int colour) {
+	}
 }
+
