57c1cb6254ed6d1488f54610f5de33dfe6f3b844
phil <phil@LenovoLark.Hemiwaly.com>
2017-10-07 16:17:03 -0700

    o.o

--- Lines/src/DrawingSurface.java
+++ Lines/src/DrawingSurface.java
@@ -5,25 +5,25 @@ import processing.core.PApplet;
 public class DrawingSurface extends PApplet {
 
 	private Line l1, l2;
-	
+
 	public DrawingSurface() {
 		l1 = null;
 		l2 = null;
 	}
-	
+
 	// The statements in the setup() function 
 	// execute once when the program begins
 	public void setup() {
 
 	}
-	
+
 	// The statements in draw() are executed until the 
 	// program is stopped. Each statement is executed in 
 	// sequence and after the last line is read, the first 
 	// line is executed again.
 	public void draw() { 
 		background(255);   // Clear the screen with a white background
-		
+
 		if (l1 != null) {
 			stroke(255,0,0);
 			l1.draw(this);
@@ -32,7 +32,7 @@ public class DrawingSurface extends PApplet {
 			stroke(0,255,0);
 			l2.draw(this);
 		}
-		
+
 		if (l1 != null && l2 != null) {
 			boolean intersect = l1.intersects(l2);
 			fill(0);
@@ -41,22 +41,24 @@ public class DrawingSurface extends PApplet {
 			text(intersect+"", width/2, 50);
 		}
 	}
-	
-	
+
+
 	public void mousePressed() {
 		if (mouseButton == LEFT) {
 			l1 = new Line(mouseX,mouseY,mouseX,mouseY);
 		} else if (mouseButton == RIGHT)
 			l2 = new Line(mouseX,mouseY,mouseX,mouseY);
 	}
-	
-	
+
+
 	public void mouseDragged() {
 		if (mouseButton == LEFT) {
 			l1.setPoint2(mouseX,mouseY);
-		} else if (mouseButton == RIGHT)
+		} else if (mouseButton == RIGHT) {
+			System.out.println("was dragged");
 			l2.setPoint2(mouseX,mouseY);
+		}
 	}
-	
-	
+
+
 }
--- ShapeDemo/src/PhysicsShape.java
+++ ShapeDemo/src/PhysicsShape.java
@@ -1,55 +1,102 @@
+import java.util.concurrent.TimeUnit;
+
 import kush.shapes.Circle;
 import kush.shapes.Shape;
+import kush.shapes.Shapes1D;
 import kush.shapes.Shapes2D;
 import processing.core.PApplet;
 
 public class PhysicsShape {
-	
+
 	//fields
-	private Shape boundingShape;
-	
+	private Shape bound;
+
 	private double vx, vy;
-	private int typeOfShape; //keeps track of type of shape
-	
-	
+
 	//constructors
-	public PhysicsShape(Shapes2D boundingShape) {
+	public PhysicsShape(Shape boundingShape) {
 		vx = 0;
 		vy = 0;
-		
-		this.boundingShape = boundingShape;
+
+		this.bound = boundingShape;
 	}
-		
-	
+
 	//methods
 	public void draw(PApplet drawer) {
-		Shapes2D newShape = (Shapes2D)boundingShape;
-		newShape.draw(drawer);
+		if(bound instanceof Shapes2D) {
+			Shapes2D bound2d = (Shapes2D)bound;
+			bound2d.draw(drawer);
+		}
+
+		else if(bound instanceof Shapes1D) {
+			Shapes1D bound1d = (Shapes1D)bound;
+			bound1d.draw(drawer);
+		}
 	}
-	
+
 	public Shape getBoundingShape() {
-		return boundingShape;
+		return bound;
 	}
-	
+
 
 	//circle will return to starting position if dragged
-	public void returnCircle(Circle circle) {
-		circle.getX();
-		circle.getY();
-		
-		
-		
-		
+	public void returnToStart (Circle circle, int startX, int startY) {
+		int x = (int) circle.getX();
+		int y = (int) circle.getY();	
+
+		//x is being pulled to the right;
+		if(x < startX) {
+
+		}
+		//x is being pulled to the left
+		if(x>startX);
+		//y is being pulled down
+		if(y>startY);
+		//y is being pulled up
+		if(y<startY);
+
 	}
-	
+
+	//for line acc = g sin(tan-1 y/x)
+	//for normal velocity for dragged circle = v1 = 4, a = -0.8
 	public void setVelocity(double vx, double vy) {
 		this.vx = vx;
 		this.vy = vy;
 	}
-	
+
+	//stops, sets all velocity to zero
+	public void stop() {
+		vx = 0;
+		vy = 0;
+	}
+
+	//for circle movement
 	public void act() {
 		//boundingShape.moveBy(boundingShape.getX(), boundingShape.getY());
-		boundingShape.moveBy(vx, vy);
+
+		if(bound instanceof Shapes2D) {
+			Shapes2D bound2d = (Shapes2D) bound;
+			//bound2d.moveBy(vx, vy);
+
+			double ax = vx/-10.0;
+			double ay = vy/-10.0;
+			
+			while(Math.abs(vx) > 0.5 || Math.abs(vy) > 0.5) {
+				bound2d.move(bound2d.getX()+vx, bound2d.getY()+vy);
+				vx += ax;
+				vy += ay;
+				System.out.println("vx is " + vx + " vy is " + vy);
+			}
+
+
+			/*try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} */
+
+		}
 	}
-	
+
 }
