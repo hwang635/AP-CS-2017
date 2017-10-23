30aae60b7da66d13c0ba937ed696a5cd1e15c940
hwang635 <hwang635@PR25.hhs.win.fuhsd.org>
2017-10-23 14:25:33 -0700

    hw: finish checkmail

---
+++ CheckMail/src/CheckMail.java
@@ -0,0 +1,10 @@
+
+public class CheckMail {
+
+	public static void main(String[]args) {
+		//take in 4 numbers
+		
+		//make package obj
+	}
+
+}
---
+++ CheckMail/src/Package.java
@@ -0,0 +1,13 @@
+
+public class Package {
+	
+	private double dim1, dim2, dim3, weight;
+	
+	public Package(double dim1, double dim2, double dim3, double weight) {
+	}
+	
+	public void checkStatus() {
+		
+	}
+
+}
