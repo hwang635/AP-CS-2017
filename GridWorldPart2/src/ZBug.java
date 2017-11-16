8bdadbcccd9a2b5d15ab30c9df2b90b94cc34d3a
phil <phil@LenovoLark.Hemiwaly.com>
2017-11-15 20:09:56 -0800

    buge

--- GridWorldPart2/src/ZBug.java
+++ GridWorldPart2/src/ZBug.java
@@ -1,20 +1,57 @@
+import info.gridworld.actor.Bug;
 import info.gridworld.grid.Location;
 
-public class ZBug extends BoxBug{
+public class ZBug extends Bug{
+
+	int steps, turns, sideLength;
 
 	public ZBug(int length) {
-		super(length);
-	}
-	
-	public void turn() {
-		//int y = 0;
-		
-		super.turn();
-		setDirection(getDirection() + Location.HALF_RIGHT);
-		//y++;
-		
-		//(if(turn == 2 && steps == length) {
-		//}
+		steps = 0;
+		turns = 1;
+		sideLength = length;
 	}
 
+	public boolean canMove()
+	{	
+		if(super.canMove()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void move() {		
+		super.move();
+
+		/*if(steps == -1) {
+			Zturn();
+			steps = 0;
+		}
+		else */ if(steps == sideLength && turns <= 2) {
+			Zturn();
+			steps--;
+		}
+
+		steps++;
+	}
+
+	public void Zturn() {
+		 if(turns == 1) {
+			super.turn();
+			super.turn();
+			super.turn();
+		}
+		else if(turns == 2) {
+			super.turn();
+			setDirection(getDirection() - 4*Location.HALF_RIGHT);
+		}
+
+		turns++;
+		steps = 0;
+	}
+
+	public void turn() {
+		super.turn();
+		steps = 0;
+	}
 }
