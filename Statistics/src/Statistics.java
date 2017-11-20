38dc1c8d723bf97c6d3734f7cc75cb1575f0789a
hwang635 <hwang635@PR32.hhs.win.fuhsd.org>
2017-11-20 14:24:23 -0800

    asd

--- Statistics/src/Statistics.java
+++ Statistics/src/Statistics.java
@@ -1,4 +1,17 @@
 
 public class Statistics {
 
+	private int[] data;
+	
+	public Statistics(int maxLength) {
+		//initialises data to length maxLength
+	}
+	
+	public void readData(String filename) {
+		//creates an ArrayReader obj, uses fillArray method to get data from file
+	}
+	
+	public void printData() {
+		
+	}
 }
