c466e9bc06b22564d990424adb80cefe12421830
phil <phil@LenovoLark.Hemiwaly.com>
2017-11-24 15:24:11 -0800

    asdasdasd

--- Statistics/src/StatisticsTester.java
+++ Statistics/src/StatisticsTester.java
@@ -4,7 +4,7 @@ public class StatisticsTester {
 	public static void main(String[] args) {
 
 		Statistics tester = new Statistics(10000);
-		tester.readData("numbers.txt");
+		tester.readData("numbers5.txt");
 		
 		double avg = tester.calcAverage();
 		double sd = tester.calcStdDev();
@@ -13,7 +13,7 @@ public class StatisticsTester {
 		System.out.println("Average is " + avg);
 		System.out.println("Standard deviation is " + sd);
 		
-		System.out.println("Mode = ");
+		System.out.println("Mode/s = ");
 		System.out.println(mode[0]);
 		for(int i = 1; i<mode.length; i++) {
 			if (!(mode[i] == mode[i-1]))
