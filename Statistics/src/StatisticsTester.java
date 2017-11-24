8eaf3766cf8d7804bc0da26cbe6a6f367169ff1f
phil <phil@LenovoLark.Hemiwaly.com>
2017-11-23 17:13:35 -0800

    ahh

--- Statistics/src/StatisticsTester.java
+++ Statistics/src/StatisticsTester.java
@@ -4,7 +4,7 @@ public class StatisticsTester {
 	public static void main(String[] args) {
 
 		Statistics tester = new Statistics(10000);
-		tester.readData("numbers.txt");
+		tester.readData("numbers2.txt");
 		
 		double avg = tester.calcAverage();
 		double sd = tester.calcStdDev();
