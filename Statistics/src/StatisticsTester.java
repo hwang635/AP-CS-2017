d5c2955ccadbfd6a977176920fdfa69f5cdf6df0
hwang635 <hwang635@PR32.hhs.win.fuhsd.org>
2017-11-29 13:37:38 -0800

    as

--- Statistics/src/StatisticsTester.java
+++ Statistics/src/StatisticsTester.java
@@ -4,7 +4,7 @@ public class StatisticsTester {
 	public static void main(String[] args) {
 
 		Statistics tester = new Statistics(10000);
-		tester.readData("numbers5.txt");
+		tester.readData("numbers4.txt");
 		
 		double avg = tester.calcAverage();
 		double sd = tester.calcStdDev();
