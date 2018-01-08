4ea71fa557e83dc8e135aacdc9476344ba14c352
phil <phil@LenovoLark.Hemiwaly.com>
2018-01-07 20:18:58 -0800

    irregular polygon done

--- IrregularPolygon/src/DrawingSurface.java
+++ IrregularPolygon/src/DrawingSurface.java
@@ -1,32 +1,45 @@
 
-
-
 import java.awt.event.KeyEvent;
 import java.awt.geom.Point2D;
 
 import processing.core.PApplet;
 
-
-
 public class DrawingSurface extends PApplet {
 
 	private IrregularPolygon poly;
-	
+
 	private int ANIMATION_TIME = 100;
 	private float x,y,time;
-	
+
 	public DrawingSurface() {
 		poly = new IrregularPolygon();
+		/*Point2D.Double point1 = new Point2D.Double(20,10);
+		Point2D.Double point2 = new Point2D.Double(70,20);
+		Point2D.Double point3 = new Point2D.Double(50,50);
+		Point2D.Double point4 = new Point2D.Double(0,40); */
+
+		Point2D.Double point1 = new Point2D.Double(120,10);
+		Point2D.Double point2 = new Point2D.Double(170,20);
+		Point2D.Double point3 = new Point2D.Double(150,50);
+		Point2D.Double point4 = new Point2D.Double(60,40);
+				
+		poly.add(point1);
+		poly.add(point2);
+		poly.add(point3);
+		poly.add(point4);	
+
+		//poly.scale(3.5);
+		poly.setStrokeWeight(10);
 		
 		runSketch();
 	}
-	
+
 	// The statements in the setup() function 
 	// execute once when the program begins
 	public void setup() {
 		//size(0,0,PApplet.P3D);
 	}
-	
+
 	// The statements in draw() are executed until the 
 	// program is stopped. Each statement is executed in 
 	// sequence and after the last line is read, the first 
@@ -36,10 +49,10 @@ public class DrawingSurface extends PApplet {
 		fill(255);
 		textAlign(CENTER);
 		textSize(12);
-		
+
 		if (poly != null) {
 			poly.draw(this);
-		
+
 			fill(0);
 			text("Perimeter: " + poly.calcPerimeter() + "\nArea: " + poly.calcArea(),(float)width/2,(float)20);
 		}
@@ -50,8 +63,8 @@ public class DrawingSurface extends PApplet {
 		}
 
 	}
-	
-	
+
+
 	public void mousePressed() {
 		if (mouseButton == LEFT) {
 			poly.add(new Point2D.Double(mouseX,mouseY));
@@ -59,17 +72,31 @@ public class DrawingSurface extends PApplet {
 			y = mouseY;
 			time = ANIMATION_TIME;
 		} 
+		else { //if right click, end point of polygon
+			time = ANIMATION_TIME;
+			poly.setEnd(true); //tells it that this is the end point
+		}
 	}
-	
-	
-	
+
+
+
 	public void keyPressed() {
 		if (keyCode == KeyEvent.VK_SPACE) {
 			poly = new IrregularPolygon();
 		} 
+		
+		if(keyCode == KeyEvent.VK_UP) { //testing bc no mouse on laptop
+			time = ANIMATION_TIME;
+			
+			poly.setEnd(true);
+		}
+		if(keyCode == KeyEvent.VK_DOWN) { //testing bc no mouse on laptop
+			time = ANIMATION_TIME;
+			poly.setEnd(false);
+		}
 	}
-	
-	
+
+
 }
 
 
--- IrregularPolygon/src/IrregularPolygon.java
+++ IrregularPolygon/src/IrregularPolygon.java
@@ -6,10 +6,15 @@ import processing.core.PApplet;	// for Processing
 
 public class IrregularPolygon extends Shape {
 	private ArrayList <Point2D.Double> myPolygon;
+	private boolean endPolygon;
+	private int weight;
 
 	// constructors
 	public IrregularPolygon() {  
 		super(0,0);
+		myPolygon = new ArrayList<Point2D.Double>();
+		endPolygon = false;
+		weight = 1;
 	}
 
 	// public methods
@@ -17,32 +22,114 @@ public class IrregularPolygon extends Shape {
 		myPolygon.add(aPoint);
 	}
 
-	public void draw(PApplet marker) {  
-		//for(Point2D.Double x: myPolygon)
+	//join adjacent points w/ line segments
+	//join end + start
+	//if right click, the end and start points connect + shape is complete
+	public void draw(PApplet marker) {
+		marker.strokeWeight(weight);
+		
+		if(myPolygon.size() == 1) {
+			marker.point((float)myPolygon.get(0).getX(),(float)myPolygon.get(0).getY());
+		}
+		else if (myPolygon.size()>1) {
+			for(int i = 0; i<myPolygon.size()-1; i++) {
+				double x1 = myPolygon.get(i).getX();
+				double y1 = myPolygon.get(i).getY();
+				double x2 = myPolygon.get(i+1).getX();
+				double y2 = myPolygon.get(i+1).getY();
+
+				marker.line((float)x1, (float)y1, (float)x2, (float)y2);
+
+			}
+			if(endPolygon) { //if endPolygon == true
+				double x1 = myPolygon.get(0).getX();
+				double y1 = myPolygon.get(0).getY();
+				double endX = myPolygon.get(myPolygon.size()-1).getX();
+				double endY = myPolygon.get(myPolygon.size()-1).getY();
+
+				marker.line((float)x1, (float)y1, (float)endX, (float)endY);
+			}
+		}
+
+		super.draw(marker);
 	}
 
 	public double calcPerimeter() { 
-		for(int i = 0; i<myPolygon.size(); i++) {
-			//int distance = 
-			
+		double perimeter = 0;
+		double x1, y1, x2, y2;
+		for(int i = 0; i<myPolygon.size()-1; i++) {
+			x1 = myPolygon.get(i).getX();
+			y1 = myPolygon.get(i).getY();
+			x2 = myPolygon.get(i+1).getX();
+			y2 = myPolygon.get(i+1).getY();
+
+			perimeter += Math.pow((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2), 0.5);
 		}
-		
-		return 0;
+
+		//endpoint - start point distance
+		x1 = myPolygon.get(0).getX();
+		y1 = myPolygon.get(0).getY();
+		x2 = myPolygon.get(myPolygon.size()-1).getX();
+		y2= myPolygon.get(myPolygon.size()-1).getY();
+
+		perimeter += Math.pow((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2), 0.5);
+
+		return perimeter;
 	}
 
 	public double calcArea() {  
-		return 0;  
+		double area = 0;
+		int length = myPolygon.size();
+		if(length<3)
+			return area;
+		else {
+			for(int i = 0; i<length-1; i++) {
+				area += myPolygon.get(i).getX()*myPolygon.get(i+1).getY();
+			}
+			area += myPolygon.get(length-1).getX()*myPolygon.get(0).getY();
+
+			for(int i = 0; i<length-1; i++) {
+				area -= myPolygon.get(i).getY()*myPolygon.get(i+1).getX();
+			}
+			area -= myPolygon.get(length-1).getY()*myPolygon.get(0).getX();
+
+			return Math.abs(area/2);
+		}
 	}
 
+	//choice methods
 	@Override
 	public boolean isPointInside(double x, double y) {
-		// TODO Auto-generated method stub
+		for(Point2D.Double p: myPolygon) {
+			//if()
+		}
 		return false;
+	}
+
+	//scales the shape according to the factor entered
+	//the first point in the arraylist stays the same, is a reference point
+	public void scale(double factor) {
+		double refX = myPolygon.get(0).getX(); //reference x and y points that stay the same
+		double refY = myPolygon.get(0).getY();
+
+		for(Point2D.Double p: myPolygon) {
+			double newX = refX + (p.getX()-refX)*factor;
+			double newY = refY + (p.getY()-refY)*factor;
+
+			p.setLocation(newX, newY);
+		}
+	}
+
+	public void setEnd(boolean endPolygon) {
+		this.endPolygon = endPolygon;
+	}
+	
+	public void setStrokeWeight(int weight) {
+		this.weight = weight;
+		System.out.println("weight = " + weight);
 	}
 
 	@Override
 	public void changeColour(boolean isGrey) {
-		// TODO Auto-generated method stub
-
 	}
 }
