6c2d8fcdaaeb61e4fbade975a4a4ba3f5f20ef97
hwang635 <hwang635@PR26.hhs.win.fuhsd.org>
2018-01-26 13:22:33 -0800

    aaaaaaaaaaaaaaa

---
+++ RecursionPractice/src/RecursionPractice.java
@@ -0,0 +1,54 @@
+
+
+public class RecursionPractice {
+
+	public static int triangleNumber(int n)
+	{
+		if (n == 1)
+			return 1;
+		else
+			return n + triangleNumber(n-1);
+	}
+
+	public static int squareNumber(int n) {
+		if(n == 1) 
+			return 1;
+		else {
+			return squareNumber(n-1) + 2*n -1;
+		}
+	}
+
+
+	public static int logBase2(int n) {
+		if(n == 1) 
+			return 0;
+		else {
+			return 1 + logBase2(n/2);
+		}
+	}
+
+
+	public static int pow(int n) {
+		if(n == 0) 
+			return 1;
+		else {
+			return 2*pow(n-1);
+		}
+	}
+
+	public static int pentagonalNumber(int n) {
+		if(n == 1)
+			return 1;
+		else {
+			return n*2 + n-2 + 1 + pentagonalNumber(n-1);
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 2;
+		int test = triangleNumber(n);
+		System.out.println("The " + n + "th triangular number is " + test);
+	}
+
+
+}
