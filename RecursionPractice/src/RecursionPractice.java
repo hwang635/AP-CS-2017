a5d08ba22096a0281224957cfe85a28891c33fb9
phil <phil@LenovoLark.Hemiwaly.com>
2018-01-29 20:40:56 -0800

    ahhhhhhhhhhhhh

--- RecursionPractice/src/RecursionPractice.java
+++ RecursionPractice/src/RecursionPractice.java
@@ -44,14 +44,45 @@ public class RecursionPractice {
 		}
 	}
 
+	public static int Fibonacci(int n) {
+		if(n == 0 || n == 1)
+			return n;
+		else {
+			return Fibonacci(n-1) + Fibonacci(n-2);
+		}
+	}
+
+	public static int FibonacciLoop(int n) {
+		int num0 = 0;
+		int num1 = 1;
+		int numN = 0;
+
+		if(n == 0 || n == 1)
+			numN = n;
+		else {
+			for(int i = 1; i<n; i++) {
+				numN = num0 + num1;
+				num0 = num1;
+				num1 = numN;
+			}
+		}
+			
+		return numN;
+	}
+	
 	public static void main(String[] args) {
 		int n = 5;
 		int test = triangleNumber(n);
 		System.out.println("The " + n + "th triangular number is " + test);
-		
+
 		/*int n = 4;
 		int test = pentagonalNumber(n);
 		System.out.println("The " + n + "th pentagonal number is " + test); */
+
+		for(int i = 0; i<12; i++) {
+			System.out.print(Fibonacci(i) + " ");
+		}
+		//System.out.print(FibonacciLoop(5) + " ");
 	}
 
 }
