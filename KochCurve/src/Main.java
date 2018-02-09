1ed63ca66e6aa33caf5daf81461a7cddce027689
hwang635 <hwang635@PR26.hhs.win.fuhsd.org>
2018-02-09 13:17:16 -0800

    1

--- KochCurve/src/Main.java
+++ KochCurve/src/Main.java
@@ -8,10 +8,6 @@ import processing.core.PApplet;
 
 public class Main {
 
-	
-	
-	
-
 	public static void main(String args[]) {
 		DrawingSurface drawing = new DrawingSurface();
 		PApplet.runSketch(new String[]{""}, drawing);
