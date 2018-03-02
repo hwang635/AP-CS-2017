400a69a4551506d56022bf2a979f5270c4021a55
hwang635 <hwang635@PR26.hhs.win.fuhsd.org>
2018-03-02 13:19:39 -0800

    koch and box

--- KochCurve/src/DrawingSurface2.java
+++ KochCurve/src/DrawingSurface2.java
@@ -4,17 +4,19 @@ import processing.core.PApplet;
 import processing.event.MouseEvent;
 
 
-public class DrawingSurface extends PApplet {
+public class DrawingSurface2 extends PApplet {
 
-	private KochCurve curve;
+	//private KochCurve curve;
 	//private KochSnowflake curve;
+	private BoxFractal box;
 	private int level, length;
 
-	public DrawingSurface() {
+	public DrawingSurface2() {
 		level = 0;
 		length = 100;
-		curve = new KochCurve (level,length);
+		//curve = new KochCurve (level,length);
 		//curve = new KochSnowflake (level,length);
+		box = new BoxFractal(level, length);
 	}
 
 	// The statements in the setup() function 
@@ -35,7 +37,7 @@ public class DrawingSurface extends PApplet {
 		text("Use the mouse wheel to change length, use UP/DOWN keys to change level.",0,15);
 
 		stroke(0);
-		curve.draw(this);		
+		box.draw(this);		
 	}
 
 
@@ -44,7 +46,7 @@ public class DrawingSurface extends PApplet {
 		length -= num*10;
 		//curve = new KochCurve(level,length);
 		//curve = new KochSnowflake(level,length);
-		curve = new KochCurve(level, length);
+		box = new BoxFractal(level, length);
 	}
 
 	public void keyPressed() {
@@ -52,11 +54,11 @@ public class DrawingSurface extends PApplet {
 			level++;
 			//curve = new KochCurve(level,length);
 			//curve = new KochSnowflake(level,length);
-			curve = new KochCurve(level, length);
+			box = new BoxFractal(level, length);
 		} else if (keyCode == KeyEvent.VK_DOWN) {
 			level--;
 			//curve = new KochCurve(level,length);
-			curve = new KochCurve(level, length);
+			box = new BoxFractal(level, length);
 		}
 	}
 
