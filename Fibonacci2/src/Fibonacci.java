37b9d298f059ea0cda2b1e56036d05ea9ddd001e
hwang635 <hwang635@PR10.hhs.win.fuhsd.org>
2018-03-09 14:14:23 -0800

    fibonacci

---
+++ Fibonacci2/src/Fibonacci.java
@@ -0,0 +1,46 @@
+/**
+ * @(#)Fibonacci.java
+ *
+ *
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	//20th Fibonacci number is 6765
+	public static long computeFibonacci(int x) {
+
+		if(x<0) 
+			throw new IllegalArgumentException("Input must be greater than 0");
+		if (x <= 1) {
+			return x;
+		} else {
+			long answer = computeFibonacci(x-2) + computeFibonacci(x-1);
+			return answer;
+		}
+
+	}
+
+	public static void main (String[] args) {		
+		try {
+			Scanner kboard = new Scanner (System.in);
+			System.out.print("Which fibonacci number would you like to find? --> ");
+			int x = kboard.nextInt();
+			long answer = computeFibonacci(x);
+			System.out.println ("The " + x + " fibonacci number is " + answer + ".");
+		}
+		catch(InputMismatchException exception) {
+			System.out.println("Input must be an integer");
+			main(args);
+		}
+		catch(StackOverflowError exception) {
+			System.out.println("Input is too large");
+		}
+	}
+
+}
+
+
+
