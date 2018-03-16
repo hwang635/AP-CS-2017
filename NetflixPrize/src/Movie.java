5408d38ac11d83a9a6eec7300573329378374958
hwang635 <hwang635@PR10.hhs.win.fuhsd.org>
2018-03-16 14:14:24 -0700

    parseMovie

--- NetflixPrize/src/Movie.java
+++ NetflixPrize/src/Movie.java
@@ -1,8 +1,28 @@
 
 public class Movie {
 	
+	public int movieID;
 	private String title;
+	private int year;
 	private String[] genre;
-	private int movieID, year;
+	
+	public Movie(int movieID, String title, int year, String[] genre) {
+		this.movieID = movieID;
+		this.title = title;
+		this.year = year;
+		this.genre = genre;
+	}
+	
+	//to String method
+	public String toString() {
+		String output = movieID + ", " + title + "(" + year + "), ";
+		
+		for(int i = 0; i<genre.length-1; i++) {
+			output += (genre [i] + "|");
+		}
+		output += genre[genre.length-1];
+		
+		return output;
+	}
 	
 }
--- NetflixPrize/src/MovieLensCSVTranslator.java
+++ NetflixPrize/src/MovieLensCSVTranslator.java
@@ -2,6 +2,37 @@
 public class MovieLensCSVTranslator {
 
 	public Movie parseMovie(String line) {
-		return Movie;
+		//extract pieces of data from the line (by looking for commas)
+		int comma = line.indexOf(",");
+		String id = line.substring(0, comma);
+		line = line.substring(comma+1);
+		
+		comma = line.indexOf(",");
+		String title = "";
+		int year = 0;
+		if(line.charAt(1) == 34) {//34 = double quotes, ascii
+			int endQuote = line.indexOf(34, 2);
+			title = line.substring(0, endQuote);
+			
+			int p2 = line.lastIndexOf(")", endQuote);
+			int p1 = line.lastIndexOf("(", endQuote);
+			year = Integer.parseInt(line.substring(p1 + 1, p2));
+		}
+		else {
+			title = line.substring(0, comma);
+		
+			int p2 = line.lastIndexOf(")", comma);
+			int p1 = line.lastIndexOf("(", comma);
+			
+			year = Integer.parseInt(line.substring(p1 + 1, p2));
+		}
+		
+		line.substring(comma+1);
+		
+		
+		//construct movie obj
+		
+		//return obj
+		return null;
 	}
 }
