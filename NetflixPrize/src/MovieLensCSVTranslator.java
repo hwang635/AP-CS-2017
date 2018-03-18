0a47ed4d5e527701041e648b808fd63779d92dca
phil <phil@LenovoLark.Hemiwaly.com>
2018-03-17 17:38:02 -0700

    parse

--- NetflixPrize/src/MovieLensCSVTranslator.java
+++ NetflixPrize/src/MovieLensCSVTranslator.java
@@ -4,35 +4,72 @@ public class MovieLensCSVTranslator {
 	public Movie parseMovie(String line) {
 		//extract pieces of data from the line (by looking for commas)
 		int comma = line.indexOf(",");
-		String id = line.substring(0, comma);
+		System.out.println(line);
+
+		int id = Integer.parseInt(line.substring(0, comma));
+		System.out.println(id);
+
 		line = line.substring(comma+1);
-		
+		System.out.println(line);
+
 		comma = line.indexOf(",");
 		String title = "";
 		int year = 0;
 		if(line.charAt(1) == 34) {//34 = double quotes, ascii
 			int endQuote = line.indexOf(34, 2);
-			title = line.substring(0, endQuote);
-			
+
 			int p2 = line.lastIndexOf(")", endQuote);
 			int p1 = line.lastIndexOf("(", endQuote);
-			year = Integer.parseInt(line.substring(p1 + 1, p2));
-		}
-		else {
-			title = line.substring(0, comma);
-		
-			int p2 = line.lastIndexOf(")", comma);
-			int p1 = line.lastIndexOf("(", comma);
+			
+			title = line.substring(0, p1-1);
+			System.out.println(title);
 			
 			year = Integer.parseInt(line.substring(p1 + 1, p2));
+			System.out.println(year);
+		} 
+		else { 
+			title = line.substring(0, comma);
+			System.out.println(title);
+
+			int p2 = line.lastIndexOf(")", comma);
+			int p1 = line.lastIndexOf("(", comma);
+
+			year = Integer.parseInt(line.substring(p1 + 1, p2));
+			System.out.println(year);
 		}
-		
+
 		line.substring(comma+1);
-		
-		
+		String[] genres = numGenres(line);
+
 		//construct movie obj
-		
-		//return obj
-		return null;
+		Movie m = new Movie(id, title, year, genres);
+		System.out.print(m);
+
+		return m;
+	}
+
+	private String[] numGenres(String line) {
+		int i = line.indexOf("|");
+		int count = 1;
+		while(i != -1) {
+			i = line.indexOf("|", i);
+			count++;
+		}
+
+		String[] genres = new String[count];
+		int start = 0;
+		for(int x = 0; x<count; x++) {
+			int end = line.indexOf("|", start);
+
+			if(end != -1)
+				genres[x] = line.substring(start, end);
+			else //last one, not ended w/ |
+				genres[x] = line.substring(start, line.length());
+				
+			start = end;
+		}
+
+		return genres;
+
 	}
 }
--- NetflixPrize/src/Tester.java
+++ NetflixPrize/src/Tester.java
@@ -5,19 +5,24 @@ public class Tester {
 	public static void main(String[] args) {
 		ArrayList<String> movieStrings =  FileIO.readFile("data" + FileIO.fileSeparator + "movies.csv");
 		for(String s : movieStrings) {
-			System.out.println(s);
+			//System.out.println(s);
 		}		
 
 		ArrayList<Movie> movieData = new ArrayList<Movie>();
 		MovieLensCSVTranslator translator = new MovieLensCSVTranslator();
-		for(String s : movieStrings) {
-			Movie m = translator.parseMovie(s);
+		//Movie m = translator.parseMovie(movieStrings.get(1));
+
+		for(int i = 1; i<movieStrings.size(); i++) {
+			Movie m = translator.parseMovie(movieStrings.get(i));
 			movieData.add(m);
 		}
-		
+		//System.out.println(m);
+
 		for(Movie m : movieData) {
 			System.out.println(m);
 		}
 	}
 
+	
+	
 }
