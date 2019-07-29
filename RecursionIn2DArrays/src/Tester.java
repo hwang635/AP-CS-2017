55ed44c6836a86753380b95e2467a8b81e1bdc5a
helen <helen@LenovoLark.hsd1.ca.comcast.net>
2019-07-29 01:38:47 -0700

    test

--- RecursionIn2DArrays/src/Tester.java
+++ RecursionIn2DArrays/src/Tester.java
@@ -2,6 +2,7 @@
 public class Tester {
 	
 	public static void main(String[] args) {
+		//prints out solution to txt file
 		Maze grid = new Maze("griddata\\test3.txt");
 		grid.solve();
 		System.out.println(grid);
